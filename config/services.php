215aa497866dfb2e1b26427f1d08ab6af8b44590
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-11-22 22:48:12 -0500

    basic consumption user list with trait

--- config/services.php
+++ config/services.php
@@ -12,7 +12,7 @@ return [
     | default location for this type of information, allowing packages
     | to have a conventional place to find your various credentials.
     |
-    */
+     */
 
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
@@ -38,6 +38,10 @@ return [
             'secret' => env('STRIPE_WEBHOOK_SECRET'),
             'tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300),
         ],
+    ],
+
+    'reqresin' => [
+        'base_uri' => env('REQUESTIN_SERVICE_URL'),
     ],
 
 ];
